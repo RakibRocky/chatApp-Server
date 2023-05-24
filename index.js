4ce2208c944da8a0750a77213bd907839f9a9c79
Rocky <rakibul.git.islam@gmail.com>
2023-05-24 22:25:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const { Socket } = require("dgram");
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (Socket) => {
+  console.log(`User Connected:${Socket.id}`);
+
+  Socket.on("join_room", (data) => {
+    Socket.join(data);
+    console.log(`User Connected:${Socket.id} with room ${data}`);
+  });
+  Socket.on("send_message", (data) => {
+    Socket.to(data.room).emit("receive_message", data);
+    console.log(data);
+  });
+  Socket.on("disconnect", () => {
+    console.log("User disconnectedd", Socket.id);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server Running");
+});
