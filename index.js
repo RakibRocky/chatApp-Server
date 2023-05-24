c7fdf45e1ad5419807d824d88f71c9415f6cafe6
Rocky <rakibul.git.islam@gmail.com>
2023-05-24 23:41:31 +0600

    serverlInk

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://splendid-entremet-e9f74d.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
